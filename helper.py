338c086652e2963a351c0380b94ac0c7ba3b9dcb
Sanjeev Yadav <sanjeev.yadav.ai@outlook.com>
2019-01-17 13:21:09 +0530

    Add gender distribution function
    Plot distribution of churn based on the gender of user

--- helper.py
+++ helper.py
@@ -1,3 +1,6 @@
+import plotly.graph_objs as go
+import plotly.offline as offline_py
+
 def spark_shape(df):
     """
     Return a tuple representing the dimensionality of the Spark Dataframe
@@ -11,3 +14,28 @@ def spark_shape(df):
     n_rows, n_cols = df.count(), len(df.columns)
     shape = (n_rows, n_cols)
     return shape
+
+def gender_dist(df):
+    """
+    Plots distribution of churn based on gender
+    """
+    trace_c = go.Bar(
+        x = df.query(' isChurn ')['gender'],
+        y = df.query(' isChurn ')['count'],
+        name = "Churn"
+    )
+
+    trace_nc = go.Bar(
+        x = df.query(' not isChurn ')['gender'],
+        y = df.query(' not isChurn ')['count'],
+        name = "Not Churn"
+    )
+
+    data = [trace_c, trace_nc]
+
+    layout = go.Layout(
+        barmode= 'group'
+    )
+
+    fig = go.Figure(data = data, layout = layout)
+    offline_py.iplot(fig)
